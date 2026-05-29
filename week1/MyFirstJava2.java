de540937ab7369ab9639268b29bfa0186ee91c62
AdilJayasinghe <meetofficialaron@gmail.com>
2026-05-29 14:03:47 +0530

    Revert "demonstation"
    This reverts commit 18b9a8bbfb81731298659f6662391c661fd20a63.

--- week1/MyFirstJava2.java
+++ week1/MyFirstJava2.java
@@ -4,7 +4,7 @@ public class MyFirstJava2
 	public static void main (String[] args)
 	{
 		Scanner input = new Scanner(System.in);
-		System.out.println("Please insert your name Bro: ");
+		System.out.println("Please insert your name: ");
 		String name = input.next();
 		System.out.println("Hello " + name);
 	}
